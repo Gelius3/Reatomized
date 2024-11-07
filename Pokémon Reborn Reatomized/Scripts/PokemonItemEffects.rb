cc3849d0aba6f3ab3288814a48c5a43c155e6efe
Gelius3 <110195781+Gelius3@users.noreply.github.com>
2024-11-07 21:20:51 +0000

    Update PokemonItemEffects.rb

--- Pokémon Reborn Reatomized/Scripts/PokemonItemEffects.rb
+++ Pokémon Reborn Reatomized/Scripts/PokemonItemEffects.rb
@@ -2447,7 +2447,7 @@ ItemHandlers::UseOnPokemon.add(:ABILITYCAPSULE,proc{|item,pokemon,scene|
   for i in abillist.keys
     next if abillist[i] == abilid
     next if abils.include?(abillist[i])
-    commands.push((i < 8 ? "" : "(H) ") + PBAbilities.getName(abillist[i]))
+    commands.push((i < 9 ? "" : "(H) ") + PBAbilities.getName(abillist[i]))
     command_option.push(i)
     abils.push(abillist[i])
   end
