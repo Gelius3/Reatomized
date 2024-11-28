988f59902d57a773341736f179f49217a2049267
Habble53 <87115317+Habble53@users.noreply.github.com>
2024-11-28 15:51:27 +0300

    Ditto

--- Pokémon Reborn Reatomized/Scripts/PokeBattle_Battler.rb
+++ Pokémon Reborn Reatomized/Scripts/PokeBattle_Battler.rb
@@ -5385,7 +5385,7 @@ class PokeBattle_Battler
 
     if @effects[PBEffects::Flinch]
       @effects[PBEffects::Flinch]=false
-      self.effects[PBEffects::Tenacity]=false if self.effects[PBEffects::Tenacity]==true
+      @effects[PBEffects::Tenacity]=false if @effects[PBEffects::Tenacity]==true
       if @battle.FE == PBFields::ROCKYF
         if !(self.ability == PBAbilities::STEADFAST) && !(self.ability == PBAbilities::STURDY) && !(self.ability == PBAbilities::INNERFOCUS) && (self.stages[PBStats::DEFENSE] < 1) && !(self.ability == PBAbilities::STURDINESS)
           @battle.pbDisplay(_INTL("{1} was knocked into a rock!",pbThis))
